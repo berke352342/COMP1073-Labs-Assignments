b89ae1f49eafdf3229bb3a73747612feaddd57ca
berkecr1TiKaL <berkcark4@gmail.com>
2026-02-10 21:07:42 +0300

    added JS to change background color dynamicaly

--- lab-2-Color-Changer/js/page-color-changer.js
+++ lab-2-Color-Changer/js/page-color-changer.js
@@ -3,16 +3,16 @@
  const greenSlider = document.getElementById('greenRange');
  const blueSlider = document.getElementById('blueRange');
 
- // STEP 2: Create a function – change the page color.
-function changeBackgroundColor() {
+// STEP 2: Create a function – change the page color.
+function changeBackgroundColor() { 
 
-     const r = redSlider.value;
-     const g = greenSlider.value;
-     const b = blueSlider.value;
-     document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
-   }
+      const r = redSlider.value;
+      const g = greenSlider.value;
+      const b = blueSlider.value;
+      document.body.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+}
 
-// STEP 3: add Event listener
+  // STEP 3: add Event listener
   redSlider.addEventListener('input', changeBackgroundColor);
   greenSlider.addEventListener('input', changeBackgroundColor);
   blueSlider.addEventListener('input', changeBackgroundColor);
